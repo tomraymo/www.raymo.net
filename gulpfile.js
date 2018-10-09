e1c13fc85b78afc5a286f05889201c6781f989e6
Tom Raymo <tom@Tom-iMac-2.local>
2018-10-09 13:47:40 -0400

    Implement imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require("gulp"),
-    sass = require("gulp-sass");
+    sass = require("gulp-sass"),
+    imagemin = require("gulp-imagemin");
 
 var outputDir = "docs",
-    sassSources = "src/sass/*.scss";
+    sassSources = "src/sass/*.scss",
+    imgSources = "src/images/*";
 
-gulp.task("sass", function() {
+gulp.task("sass", function () {
     gulp.src(sassSources)
         .pipe(
             sass({
@@ -14,8 +16,15 @@ gulp.task("sass", function() {
         .pipe(gulp.dest(outputDir + "/css"));
 });
 
-gulp.task("watch", function() {
-    gulp.watch(sassSources, ["sass"]);
+gulp.task("imagemin", function () {
+    gulp.src(imgSources)
+        .pipe(imagemin())
+        .pipe(gulp.dest(outputDir + "/img"));
 });
 
-gulp.task("default", ["sass", "watch"]);
+gulp.task("watch", function () {
+    gulp.watch(sassSources, ["sass"]);
+    gulp.watch(imgSources, ["imagemin"]);
+});
+
+gulp.task("default", ["sass", "imagemin", "watch"]);
